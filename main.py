b55529f5ce4a4114baad67ba479c16f5c61cf2b0
Cybersport <Cybersport@26kadr.internal>
2024-05-27 14:51:08 +0300

    fifth

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from keyboard.keyboards import get_keyboard_1, get_keyboard_2
 
 bot = Bot(token = TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-button1 = KeyboardButton('Отправь фото кота')
-button2 = KeyboardButton('Перейти на следующую клавиатуру')
-keyboard.add(button1, button2)
-
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
-    await message.reply('Привет, я эхо-бот', reply_markup=keyboard)
-
+    await message.reply('Привет, я второй бот', reply_markup=get_keyboard_1())
 
 @dp.message_handler(lambda message: message.text == 'Отправь фото кота')
 async def button_1_click(message: types.Message):
-    await bot.send_photo(message.chat_id)
+    await bot.send_photo(message.chat.id, photo= 'https://clck.ru/3Asn6h', caption= 'Вот тебе кот!')
 
 @dp.message_handler(lambda message: message.text == 'Перейти на следующую клавиатуру')
 async def button_2_click(message: types.Message):
-    await message.answer('Ты нажал кнопку 2')
+    await message.answer('Тут ты можешь попросить бота отправить фото собаки', reply_markup=get_keyboard_2())
+
+@dp.message_handler(lambda message: message.text == 'Отправь фото собаки')
+async def button_3_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo= 'https://clck.ru/3Aso98', caption= 'Вот тебе собака!')
+
+@dp.message_handler(lambda message: message.text == 'Перейти на первую клавиатуру')
+async def button_2_click(message: types.Message):
+    await message.answer('Тут ты можешь попросить бота отправить фото кота', reply_markup=get_keyboard_1())
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
